1512d6f238e41075750abfc64d46a31cd3f38dee
Kalina Valeva <>
2022-11-24 14:47:11 +0200

    Welcome Screen

---
+++ app/src/main/java/com/mobile/tuesplace/ui/classes/ClassesScreen.kt
@@ -0,0 +1,4 @@
+package com.mobile.tuesplace.ui.classes
+
+class ClassesScreen {
+}
--- app/src/main/java/com/mobile/tuesplace/ui/theme/Color.kt
+++ app/src/main/java/com/mobile/tuesplace/ui/theme/Color.kt
@@ -4,5 +4,8 @@ import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Blue = Color(0xFF2D31FA)
 val Teal200 = Color(0xFF03DAC5)
+val LogoBlue = Color(0xFF4585FF)
+val BabyBlue = Color(0xFFDFF6FF)
+val DarkBlue = Color(0xFF051367)
